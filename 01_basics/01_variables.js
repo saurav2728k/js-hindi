0b7dbb024f231dde70c69e187340cca39b50768a
Saurav Kumar <143354041+saurav2728k@users.noreply.github.com>
2024-10-01 02:36:10 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+Const accountId=11122;
+let accountname= "saurav";
+var accountpass=12345;
+//accountId=5555;   //not allowed
+accountcity="barh";
+console.table([accountId,accountname,accountpass,accountcity]);

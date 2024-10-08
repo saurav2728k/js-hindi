6daf87e663f52de21b0cbadc733f6651d2fc7a0c
Saurav Kumar <143354041+saurav2728k@users.noreply.github.com>
2024-10-08 13:14:59 +0000

    conversionoperation

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -4,3 +4,15 @@ console.log(typeof score)
 let valueinnumber= Number(score)
 console.log( valueinnumber)
 console.log(typeof valueinnumber)
+let str1="saurav"
+let str2="kumar"
+let str=str1+str2
+//console.log(str)
+console.log("1"+"2")
+console.log(2+"2")
+console.log("2"+2)
+console.log("2"+2+2)
+console.log(1+2+"5")
+console.log(true)
+console.log(+true)
+console.log(+" ")
